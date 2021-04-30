9dd339ad4e2a5beafe86309bec4a4ceaf786b211
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-04-30 16:04:04 +0100

    Write new function getAllProducts which returns only list of products instead of entire response

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-// Get home page
-router.get('/', async (req, res, next) => {
+async function getAllProducts() {
   try {
     const statement = `SELECT * FROM products`;
     const values = [];
 
-    const response = await db.query(statement, values);
+    const result = await db.query(statement, values);
 
+    if (result.rows?.length) {
+      return result.rows;
+    }
+    return [];
+  } catch (err) {
+    throw err;
+  }
+}
+
+// Get home page
+router.get('/', async (req, res, next) => {
+  try {
+    const response = await getAllProducts();
     res.status(200).send(response);
   } catch (err) {
     next(err);
