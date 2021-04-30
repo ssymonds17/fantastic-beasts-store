22407c2d9e1f715e972eaae891d9b290def1b049
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-04-30 15:18:06 +0100

    Rewrite basic get request to return all data from products table to establish connection with database

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ const db = require('../db');
 
 // Get home page
 router.get('/', async (req, res, next) => {
-  res.send('Hello world');
+  try {
+    const statement = `SELECT * FROM products`;
+    const values = [];
+
+    const response = await db.query(statement, values);
+
+    res.status(200).send(response);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
