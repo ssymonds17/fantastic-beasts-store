e6411eaaf31cda5faa4786f8fdb7c1db9da9d05e
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-04 11:39:11 +0100

    Add route to handle update to user account by id. Return plain text indicating user id before finalising logic

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,23 @@ var router = express.Router();
 module.exports = (app) => {
   app.use('/users', router);
 
-  /* GET user by id */
+  // Get user by id
   router.get('/:userId', function(req, res, next) {
-    const { userId } = req.params;
-    res.send(`user with id: ${userId}`);
+    try {
+      const { userId } = req.params;
+      res.send(`user with id: ${userId}`);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  // UPDATE user by id
+  router.put('/:userId', async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      res.send(`Updated user with id: ${userId}`);
+    } catch (err) {
+      next(err);
+    }
   });
 };
