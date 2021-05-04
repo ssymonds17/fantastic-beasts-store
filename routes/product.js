20cacfc2c03799b63b5f6b44677b55bcd0561873
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-04 10:53:09 +0100

    Refactor code within routes/product to remove the direct query code to the db. This code will then be imported from the models folder

--- routes/product.js
+++ routes/product.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const ProductModel = require('../models/product');
+const ProductModelInstance = new ProductModel();
 
 module.exports = (app) => {
   app.use('/products', router);
 
-  async function getAllProducts() {
-    try {
-      const statement = `SELECT * FROM products`;
-      const values = [];
-
-      const result = await db.query(statement, values);
-
-      if (result.rows?.length) {
-        return result.rows;
-      }
-      return [];
-    } catch (err) {
-      throw err;
-    }
-  }
-
   // Get home page
   router.get('/', async (req, res, next) => {
     try {
-      const response = await getAllProducts();
+      const response = await ProductModelInstance.findAll();
       res.status(200).send(response);
     } catch (err) {
       next(err);
