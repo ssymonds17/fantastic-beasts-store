dd571c0d07492367418c01d39f67de5ef1ecab7b
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-04 17:17:45 +0100

    Add express-session to the project and set up functionality to create a session. Also fix error where app was not being returned from file to be used by other loaders

--- loaders/express.js
+++ loaders/express.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const session = require('express-session');
+const { SESSION_SECRET } = require('../config');
 
 module.exports = (app) => {
   // Enable CORS to all origins by default
@@ -8,4 +10,21 @@ module.exports = (app) => {
   // Parse JSON from requests
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  // Express-session
+  app.set('trust proxy', 1); // Trust first proxy
+  // Create a session
+  app.use(
+    session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000
+      }
+    })
+  );
+
+  return app;
 };
