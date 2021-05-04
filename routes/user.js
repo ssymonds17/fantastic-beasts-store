49ffd7dc511485e2c68c086658c1834ddfb4833e
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-04 15:43:58 +0100

    Add route to handle retrieving user data based on their email

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
+const ModelUser = require('../models/user');
+const ModelUserInstance = new ModelUser();
 
 module.exports = (app) => {
   app.use('/users', router);
@@ -9,6 +12,23 @@ module.exports = (app) => {
     try {
       const { userId } = req.params;
       res.send(`user with id: ${userId}`);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  // GET user by email
+  router.get('/check/:email', async (req, res, next) => {
+    try {
+      const { email } = req.params;
+      const response = await ModelUserInstance.findOneByEmail(email);
+
+      // If no user found then reject
+      if (!response) {
+        throw createError(404, 'User not found');
+      }
+
+      res.status(200).send(response);
     } catch (err) {
       next(err);
     }
