aac8291d02fae221c725cca68e44a0f34967b503
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-11 16:31:45 +0100

    Create skeleton structure for Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import logo from '../logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-      <h3>Hello from Navbar</h3>
-    </div>
+    <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+      <Link to='/'>
+        <img
+          // TODO Change logo to relevant SVG
+          src={logo}
+          alt='store'
+          className='navbar-brand'
+          style={{ color: 'red' }}
+        />
+      </Link>
+      <ul className='navbar-nav align-items-centre'>
+        <li className='nav-item ml-5'>
+          <Link to='/' className='nav-link'>
+            products
+          </Link>
+        </li>
+      </ul>
+      <Link to='/cart' className='ml-auto'>
+        <button>
+          <i className='fas fa-cart-plus' />
+          my cart
+        </button>
+      </Link>
+    </nav>
   );
 }
