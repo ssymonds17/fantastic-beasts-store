96e578cef81958e08e8a8e66fdc197462d90ee0c
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-11 18:58:28 +0100

    Move img folder back to public as this was the correct position for it originally. Set up Product page so that each product's name, description, price and image can be viewed

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Product() {
+export default function Product({ product }) {
+  const { id, name, description, price, image } = product;
   return (
-    <div>
-      <h3>Hello from Product</h3>
+    <div className='product-wrapper col-9 mx-auto col-md-6 col-lg-3 my-3'>
+      <div className='card'>
+        <div className='img-container p-5' onClick={() => console.log(image)}>
+          <Link to='details'>
+            <img src={`img/${image}`} alt='product' className='card-img-top' />
+          </Link>
+        </div>
+        <h3>{name}</h3>
+        <h5>{description}</h5>
+        <h3>¥{price}</h3>
+      </div>
     </div>
   );
 }
