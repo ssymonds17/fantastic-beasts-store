1da3df962b1738704da2739566f8019835052cf0
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-12 11:23:01 +0100

    Migrate button-container style from app.css to new button-container.css file within components/styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
+import '../components/styles/button-container.css';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
