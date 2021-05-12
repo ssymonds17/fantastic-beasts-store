f04875386697a24c6e184242beec93a8a8066256
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-12 15:13:12 +0100

    Add style for modal in separate modal-container.css file and update classnames and id for divs within modal component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useGlobalContext } from '../context';
 import { Link } from 'react-router-dom';
 import './styles/button-container.css';
+import './styles/modal-container.css';
 
 export default function Modal() {
   const { modalOpen, modalProduct, closeModal } = useGlobalContext();
@@ -14,7 +15,10 @@ export default function Modal() {
       <div className='modal-container'>
         <div className='container'>
           <div className='row'>
-            <div className='col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize'>
+            <div
+              id='modal'
+              className='col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize'
+            >
               <h5>item added to the cart</h5>
             </div>
           </div>
