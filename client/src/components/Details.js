e1bf8925b9e0db9418f072baae935c01fbf10bd4
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-12 12:42:40 +0100

    Write details component which displays all the relevant information about a chosen product as well as two buttons: return to products page and add item to cart

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { useGlobalContext } from '../context';
+import { Link } from 'react-router-dom';
+import './styles/button-container.css';
 
 export default function Details() {
+  const { detailProduct, addToCart } = useGlobalContext();
+  const { id, name, description, price, image, inCart } = detailProduct;
+
   return (
-    <div>
-      <h3>Hello from Details</h3>
+    <div className='container py-5'>
+      {/* Name */}
+      <div className='row'>
+        <div className='col-10 mx-auto text-center my-5'>
+          <h1>{name}</h1>
+        </div>
+      </div>
+      {/* End of name */}
+      {/* Product Info */}
+      <div className='row'>
+        <div className='col-10 mx-auto col-md-6 my-3'>
+          <img src={`img/${image}`} className='img-fluid' alt='product' />
+        </div>
+        {/* Product Text */}
+        <div className='col-10 mx-auto col-md-6 my-3'>
+          <h4>
+            <strong>
+              Price : <span>¥</span>
+              {price}
+            </strong>
+          </h4>
+          <p className='font-weight-bold mt-3 mb-0'>
+            Some info about the product
+          </p>
+          <p className='text-muted lead'>{description}</p>
+          {/* Buttons */}
+          <Link to='/'>
+            <button className='button-container text-capitalize'>
+              back to products
+            </button>
+          </Link>
+          <button
+            className='button-container text-capitalize'
+            disabled={inCart ? true : false}
+            onClick={() => addToCart(id)}
+          >
+            {inCart ? 'in cart' : 'add to cart'}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/context.js
+++ client/src/context.js
@@ -4,14 +4,22 @@ const ProductContext = React.createContext();
 
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState('');
-  const [detailProduct, setDetailProduct] = useState('Default Product');
+  const [detailProduct, setDetailProduct] = useState({
+    id: 1,
+    name: 'Basilisk',
+    description:
+      'Reptile reputed to be a serpent king. Can cause death with a single glance.',
+    price: 650,
+    image: 'basilisk.png',
+    inCart: false
+  });
 
   // Functions
   const handleDetails = () => {
     console.log('Handle the details');
   };
-  const addToCart = () => {
-    console.log('Add to cart');
+  const addToCart = (id) => {
+    console.log(`Product with id of ${id} added to cart`);
   };
   return (
     <ProductContext.Provider
