d8b3d2c19b218b3d1be5f97e7279d01c11dcce9f
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-13 10:41:12 +0100

    Create cartColumns component and import into cart

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from '../Title';
+import CartColumns from './CartColumns';
 
 export default function Cart() {
   return (
     <section>
       <Title title='your cart' />
+      <CartColumns />
     </section>
   );
 }
