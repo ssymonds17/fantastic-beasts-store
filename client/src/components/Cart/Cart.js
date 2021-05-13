e6ef216284ece902f2a264349ccfa8452f00c902
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-13 10:48:11 +0100

    Create and import EmptyCart component into Cart

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
 
 export default function Cart() {
   return (
     <section>
       <Title title='your cart' />
       <CartColumns />
+      <EmptyCart />
     </section>
   );
 }
