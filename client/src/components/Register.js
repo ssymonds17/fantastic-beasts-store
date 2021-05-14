8d41a9b74a7e2484891bf7ff4a80f874836f821a
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-05-14 16:23:37 +0100

    Add login button so user can be redirected to login page if they already have an account and add stylings to registration page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { useHistory } from 'react-router-dom';
 import { registerUser } from '../apis/auth';
+import './styles/register.css';
 
 export default function Register() {
   const [error, setError] = useState('');
@@ -41,72 +42,85 @@ export default function Register() {
   };
 
   return (
-    <div className='container-fluid'>
-      <Formik
-        initialValues={{
-          first_name: '',
-          last_name: '',
-          email: '',
-          password: '',
-          confirmPassword: ''
-        }}
-        validateOnBlur
-        onSubmit={async (data) => {
-          await handleRegister(data);
-        }}
-      >
-        <Form className='text-center mx-auto'>
-          <header>
-            <h1>Register</h1>
-          </header>
-          <label htmlFor='first_name'>First Name</label>
-          <Field
-            id='first_name'
-            name='first_name'
-            placeholder='John'
-            required
-          />
-          <label htmlFor='last_name'>Last Name</label>
-          <Field id='last_name' name='last_name' placeholder='Doe' required />
-          <label htmlFor='email'>
-            Email<small>*</small>
-          </label>
-          <Field
-            id='email'
-            name='email'
-            placeholder='email@example.com'
-            required
-          />
-          <label htmlFor='password'>
-            Password<small>*</small>
-          </label>
-          <Field
-            id='password'
-            name='password'
-            placeholder='password'
-            type='password'
-            required
-          />
-          <label htmlFor='confirmPassword'>
-            Confirm Password<small>*</small>
-          </label>
-          <Field
-            id='confirmPassword'
-            name='confirmPassword'
-            placeholder='password'
-            type='password'
-            required
-          />
-          <button type='submit' className='btn btn-primary'>
-            Submit
-          </button>
-          {error && (
-            <div>
-              <strong>{error}</strong>
-            </div>
-          )}
-        </Form>
-      </Formik>
+    <div>
+      <div className='form-container'>
+        <div className='form-wrapper'>
+          <Formik
+            initialValues={{
+              first_name: '',
+              last_name: '',
+              email: '',
+              password: '',
+              confirmPassword: ''
+            }}
+            validateOnBlur
+            onSubmit={async (data) => {
+              await handleRegister(data);
+            }}
+          >
+            <Form className='form'>
+              <header className='text-center'>
+                <h1>Register</h1>
+              </header>
+              <label htmlFor='first_name'>First Name</label>
+              <Field
+                id='first_name'
+                name='first_name'
+                placeholder='John'
+                required
+              />
+              <label htmlFor='last_name'>Last Name</label>
+              <Field
+                id='last_name'
+                name='last_name'
+                placeholder='Doe'
+                required
+              />
+              <label htmlFor='email'>
+                Email<small>*</small>
+              </label>
+              <Field
+                id='email'
+                name='email'
+                placeholder='email@example.com'
+                required
+              />
+              <label htmlFor='password'>
+                Password<small>*</small>
+              </label>
+              <Field
+                id='password'
+                name='password'
+                placeholder='Password'
+                type='password'
+                required
+              />
+              <label htmlFor='confirmPassword'>
+                Confirm Password<small>*</small>
+              </label>
+              <Field
+                id='confirmPassword'
+                name='confirmPassword'
+                placeholder='Password'
+                type='password'
+                required
+              />
+              <button type='submit' className='btn btn-primary'>
+                Submit
+              </button>
+              {error && (
+                <div>
+                  <strong>{error}</strong>
+                </div>
+              )}
+            </Form>
+          </Formik>
+        </div>
+      </div>
+      <div className='text-center mt-3'>
+        <h3>Already registered?</h3>
+        <button className='btn btn-success'>Log In</button>
+      </div>
     </div>
   );
 }
