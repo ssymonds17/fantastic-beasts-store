c813385795f99f04212166ecc3573985cbdd3fc1
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-29 13:27:57 +0100

    Add button to return home from checkout success view

--- client/src/components/CheckoutSuccess.js
+++ client/src/components/CheckoutSuccess.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CheckoutSuccess() {
-  return <div>Order Successful</div>;
+  return (
+    <div className='container'>
+      <main className='success-container'>
+        <h1>Order Complete</h1>
+        <Link to='/'>
+          <button className='btn btn-success'>Return Home</button>
+        </Link>
+      </main>
+    </div>
+  );
 }
