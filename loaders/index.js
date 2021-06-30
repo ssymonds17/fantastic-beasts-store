80a1a64d18bcc6d74a7f8bd7006021392b786ac4
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-30 11:39:04 +0100

    Add declaration that serves static files

--- loaders/index.js
+++ loaders/index.js
@@ -2,6 +2,7 @@ const expressLoader = require('./express');
 const passportLoader = require('./passport');
 const routeLoader = require('../routes');
 const swaggerLoader = require('./swagger');
+const path = require('path');
 
 module.exports = async (app) => {
   // Load express middlewares
@@ -16,8 +17,10 @@ module.exports = async (app) => {
   // Load Swagger documentation
   await swaggerLoader(app);
 
-  app.get('*', (req, res) => {
-    res.send('404 Resource does not exist');
+  // Serve static files
+  app.use(express.static('client/build'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 
   // Error handler
