6f2b3387685fa18ac6a9e0d58b13baf720d4a657
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-07-02 11:40:02 +0100

    Revert backend to previous iteration where api was operating correctly

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const { PORT } = require('./config');
 const loaders = require('./loaders');
 
 async function startServer() {
   // Initialise application loaders
   loaders(app);
-
-  // Serve static files
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
 
   // Start server
   app.listen(PORT, () => {
